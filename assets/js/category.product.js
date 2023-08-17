0081e48a115ab8615eb0a024638c7b734a4174ad
Nicolly Santos <nicolly.silva5@aluno.senai.br>
2023-08-17 10:18:23 -0300

    feat: Add class Product

--- assets/js/category.product.js
+++ assets/js/category.product.js
@@ -6,3 +6,12 @@ class Category { //categoria possui varios produtos => 1:N
     }
 }
 
+class Product {
+    constructor(id, name, price, category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+}
+
